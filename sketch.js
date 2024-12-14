df6c735943004334a8017a77ddf487ae240cc75a
totibor <123023599+totibor@users.noreply.github.com>
2024-12-14 22:28:26 +0100

    Convert template to use Vite

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 /// <reference path="node_modules/@types/p5/global.d.ts" />
-/* TypeScript Triple-slash directive. It adds type and function descriptions for p5.js*/
+
+const debug = {
+  
+}
 
 function setup() {
-  // put setup code here
   createCanvas(400, 400);
 
-  // position the canvas in the middle of the window
   select("canvas").position(
     windowWidth / 2 - width / 2,
     windowHeight / 2 - height / 2
@@ -13,6 +14,5 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
   background(255);
 }
